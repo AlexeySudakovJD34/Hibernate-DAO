ba9805a8c136d3d488768ecc0438a5e48a4b1a60
AlexeySudakovJD34 <sudakovaleksey@mail.ru>
2023-09-13 13:21:24 +0300

    Second commit

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into hibernate.person (age, name, surname, city_of_living, phone_number) values (27, 'Ivan', 'Petrov', 'Moscow', null);
+insert into hibernate.person (name, age, surname, city_of_living, phone_number) values ('Ivan', 27, 'Petrov', 'Moscow', null);
 insert into hibernate.person (age, name, surname, city_of_living, phone_number) values (33, 'Olga', 'Semenova', 'Moscow', '+79990010101');
 insert into hibernate.person (age, name, surname, city_of_living, phone_number) values (23, 'Nurlan', 'Sadykov', 'Atyrau', '+77015554466');
 insert into hibernate.person (age, name, surname, city_of_living, phone_number) values (9, 'Nikolai', 'Sudakov', 'Vidnoe', null);
